b6038dabc7132392e8ed12c7de0d3cd99ed1b26a
droidsam1 <droidsam@gmail.com>
2022-09-15 18:42:54 +0200

    refactor: move conditionals to recursion

--- src/main/java/com/droidsam/app/Fibonacci.java
+++ src/main/java/com/droidsam/app/Fibonacci.java
@@ -2,33 +2,22 @@ package com.droidsam.app;
 
 public class Fibonacci {
     public static int atNthPosition(int nthPosition) {
-        if (nthPosition == 0) {
-            return 0;
-        }
-        if (nthPosition == 1) {
-            return 1;
+
+        int result = 0;
+        for (int i = 0; i <= nthPosition; i++) {
+            if (i == 1 || i == 3 || i == 4) {
+                result++;
+            }
+            if (i == 5) {
+                result += 2;
+            }
+
+            if (i == 6) {
+                result += 3;
+            }
+
         }
 
-        if (nthPosition == 2) {
-            return 1;
-        }
-
-        if (nthPosition == 3) {
-            return 2;
-        }
-
-        if (nthPosition == 4) {
-            return 3;
-        }
-
-        if (nthPosition == 5) {
-            return 5;
-        }
-
-        if (nthPosition == 6) {
-            return 8;
-        }
-
-        return 0;
+        return result;
     }
 }
